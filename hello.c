61b2037a16de4f0eb0dcba6a21dcc90c02450811
dsf900 <dsf900@gmail.com>
2020-11-12 14:56:56 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,10 +3,12 @@
 int main(){
 
   int x = 42;
+  int y = 100;
   
   printf("Hello, world!\n");
   
   printf("My number is %d\n", x );
+  printf("My second number is %d\n", y );
   
   return 0;
 
